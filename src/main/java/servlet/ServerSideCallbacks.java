c245c6c01e5c6cc6f63a4c208583b66ebf928dfc
muskumallik <muskumallik@gmail.com>
2021-02-21 09:31:39 +0530

    Update ServerSideCallbacks.java

--- src/main/java/servlet/ServerSideCallbacks.java
+++ src/main/java/servlet/ServerSideCallbacks.java
@@ -41,8 +41,8 @@ public class ServerSideCallbacks extends HttpServlet{
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws 
         ServletException, IOException {
 
-    		System.setProperty("javax.net.ssl.trustStore",".jdk/jre/lib/security/cacerts");
-    		System.setProperty("javax.net.ssl.trustStorePassword", System.getenv("CACERTS_PASSWORD"));
+    		//System.setProperty("javax.net.ssl.trustStore",".jdk/jre/lib/security/cacerts");
+    		//System.setProperty("javax.net.ssl.trustStorePassword", System.getenv("CACERTS_PASSWORD"));
 	    //   -Djavax.net.ssl.keyStore=mySrvKeystore -Djavax.net.ssl.keyStorePassword=123456
 	    System.setProperty("javax.net.ssl.keyStore","mySrvKeystore");
 		System.setProperty("javax.net.ssl.keyStorePassword","123456");
